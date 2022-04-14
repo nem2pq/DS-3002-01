fdb1fcca280c34b51e664396e45c4457c2ac55ba
Jon Tupitza <98652016+JTupitza-UVA@users.noreply.github.com>
2022-04-14 08:55:04 -0400

    Initial Commit
    Works on Community Edition

---
+++ 05-Databricks/Structured-Streaming/Includes/Common-Notebooks/Common-Test.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+# MAGIC 
+# MAGIC %md
+# MAGIC # Integration Tests
+# MAGIC The purpose of this notebook is to faciliate testing of our systems.
+
+# COMMAND ----------
+
+spark.conf.set("com.databricks.training.module-name", "common-notebooks")
+
+# COMMAND ----------
+
+# MAGIC %run ./Common
+
+# COMMAND ----------
+
+allDone(courseAdvertisements)
